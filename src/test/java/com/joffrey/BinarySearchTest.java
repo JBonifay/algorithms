85742b618fd0b4dea8dd6348bbba5a6c64829ea9
JBonifay <joffreybonifay83@gmail.com>
2022-02-01 21:54:31 +0100

    feat: add binarySearch

---
+++ src/test/java/com/joffrey/BinarySearchTest.java
@@ -0,0 +1,27 @@
+package com.joffrey;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BinarySearchTest {
+
+    private BinarySearch bs;
+
+    @BeforeEach
+    void setUp() {
+        bs = new BinarySearch();
+    }
+
+    @Test
+    void returnMinus1WhenTargetIsNotInArray() {
+        assertEquals(4, bs.search(new int[]{1, 3, 5, 7, 9}, -1));
+    }
+
+    @Test
+    void return1WhenTargetIs3AndIsAtIndex1() {
+        assertEquals(1, bs.search(new int[]{1, 3, 5, 7, 9}, 3));
+    }
+
+}
