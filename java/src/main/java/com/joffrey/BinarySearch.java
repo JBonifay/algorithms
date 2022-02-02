ace1f4ffe29328b04cd2a18ebf2665a14fb01862
JBonifay <joffreybonifay83@gmail.com>
2022-02-02 20:29:20 +0100

    refactor: move all in java folder in order to add other language next

--- java/src/main/java/com/joffrey/BinarySearch.java
+++ java/src/main/java/com/joffrey/BinarySearch.java
@@ -3,21 +3,25 @@ package com.joffrey;
 public class BinarySearch {
 
     public int search(int[] input, int target) {
-        int low = 0;
-        int high = input.length - 1;
+        // int low = 0 (because min index is 0)
+        // int high is equals to the size of the array (-1 because we start from zero !)
 
-        while (low <= high) {
-            int mid = (low + high) / 2;
-            int curr = input[mid];
-            if (curr == target) {
-                return mid;
-            } else if (curr > target) {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
-        }
-        return -1;
+        // While ( *condition )
+        // * Condition to continue the loop:
+        // low < high because of low = mid + 1;
+        // low = high because of high = mid - 1;
+
+        // We get the mid value => (low + high) / 2
+        // and the current value => input[mid]
+
+        // if current value == target
+        // return mid value
+        // else if current value > target
+        // set high to mid - 1 (-1 because we don't want to include the actual mid value in next turn !)
+        // else (if current value < target
+        // set low to mid + 1 (+1 because we do not want to include mid value in next turn !)
+
+        // return -1 because we did not found target
     }
 
 }
